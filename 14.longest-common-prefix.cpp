2c1a71dfeca01c208e879a7010bdd20149db2e34
Dru-429 <ect.dhruv@gmail.com>
2026-09-01 15:26:01 +0530

    Added neetcode 5

--- 14.longest-common-prefix.cpp
+++ 14.longest-common-prefix.cpp
@@ -8,11 +8,25 @@ using namespace std;
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        string prefix = "";
+        sort(strs.begin(), strs.end());
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
+
+        for (int i = 0; i < first.length(); i++)
+        {
+            if (first[i] != last[i])
+            {
+                return prefix;
+            }
+            prefix += first[i];
+        }
+        return prefix;
     }
 };
 // @lc code=end
-
